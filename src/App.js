5ad798237febed48f85b19be42cf22306acdaadd
Fuh Suh <tofuhsuh@gmail.com>
2022-12-24 14:10:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTask = () =>{
+    setTodoList([...todoList, todo])
+    setTodo('')
+  }
+
+  const removeTask = () =>{
+    const newTodos =[...todoList];
+    newTodos.pop()
+    setTodoList(newTodos)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Stack List</h1>
+      <div>
+        {todoList.map(e => (
+          <p key={e}>{e}</p>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={todo}
+        placeholder="type here..."
+        onChange={e => setTodo(e.target.value)}
+      />
+      <button onClick={addTask}>Add</button>
+      <button onClick={removeTask}>Pop</button>
+      <p>Top List Item(firstIn): {todoList[0]}</p>
+      <p>Stack Size: {todoList.length}</p>
+
+
     </div>
   );
 }
